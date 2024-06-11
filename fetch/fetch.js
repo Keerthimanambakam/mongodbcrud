d0eb6b4489c7480cd99f590190423df24ddabcb0
Keerthimanambakam <keerthi99055@gmail.com>
2024-06-11 14:17:18 +0530

    Initial commit

---
+++ fetch/fetch.js
@@ -0,0 +1,29 @@
+const express = require('express')
+let mongodb = require('mongodb')
+//import url
+const url = require('../url')
+//create mongo client
+let mcl = mongodb.MongoClient
+//create router instance
+let router = express.Router()
+//create rest api
+router.get("/",(req,res) => {
+    //connect to mongodb
+    mcl.connect(url,(err,conn) => {
+        if(err)
+            console.log('Error in connecting')
+        else{
+            let db = conn.db('nodedb')
+            db.collection('products').find().toArray((err,array)=>{
+                if(err)
+                    console.log('Error:-',err)
+                else{
+                    console.log('Data sent')
+                    res.json(array)
+                    conn.close()
+                }
+            })
+        }
+    })
+})
+module.exports = router;
